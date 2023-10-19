8b41673d48c23e8a84ff92c1dc4a1b4707fa56b7
Ritesh Tiwari <ritesh.tiwari@idrive.com>
2023-10-19 11:18:41 +0530

    added problem description

--- src/recursion/FindSentencesFromWord.java
+++ src/recursion/FindSentencesFromWord.java
@@ -1,9 +1,30 @@
 package recursion;
 
 public class FindSentencesFromWord {
-
+	
+//	Problem Description::
+//	    
+//		Given a String "word" and an array of Strings "words", add spaces in "word" to construct a sentence 
+//		where each word is a valid word from "words". Return all such possible sentences in any order.
+//
+//		Note: The same word in the "words" may be reused multiple times in the segmentation.
+//
+//	Example 1::
+//		Input: word = "batsandcows", words = {"bat", "bats", "and", "sand", "cows", "cow"} 
+//		Output: {"bat sand cows", "bats and cows"}
+//	
+//	Example 2::
+//		Input: word = "basand", words = {"bat", "bats", "and", "sand", "cows", "cow"} 
+//		Output: {}
+//		
+//	Constraints::
+//		1 <= word.length <= 20
+//		1 <= words.length <= 1000
+//		1 <= words[i].length <= 10
+//		Word and words[i] consist of lowercase English letters.
+//		All the strings of words array are unique.
+	
 	public static void main(String[] args) {
-		//write code for testing the solution
 				String[] words = { "bat", "bats", "and", "sand", "cows", "cow" };
 				String[] result = findProperSentences("batsandcows", words);
 				for (String str : result) {
