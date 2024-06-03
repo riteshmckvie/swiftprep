c80d71fad0296618f24faedc0971d3d45e21ab02
Ritesh Tiwari <ritesh.tiwari@idrive.com>
2024-06-03 11:13:08 +0530

    knapsack top-down

--- src/greedy/KnapsackTopDown.java
+++ src/greedy/KnapsackTopDown.java
@@ -1,23 +1,23 @@
 package greedy;
 
-public class Knapsack {
+public class KnapsackTopDown {
 
 	static int max(int a, int b) {
 		return (a > b) ? a : b;
 	}
 
 	static int knapSack(int W, int wt[], int val[], int n) {
-		int i, w;
+		int i, j;
 		int K[][] = new int[n + 1][W + 1];
 
 		for (i = 0; i <= n; i++) {
-			for (w = 0; w <= W; w++) {
-				if (i == 0 || w == 0)
-					K[i][w] = 0;
-				else if (wt[i - 1] <= w)
-					K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+			for (j = 0; j <= W; j++) {
+				if (i == 0 || j == 0)
+					K[i][j] = 0;
+				else if (wt[i - 1] <= j)
+					K[i][j] = max(val[i - 1] + K[i - 1][j - wt[i - 1]], K[i - 1][j]);
 				else
-					K[i][w] = K[i - 1][w];
+					K[i][j] = K[i - 1][j];
 			}
 		}
 
